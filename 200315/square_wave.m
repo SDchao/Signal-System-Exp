7445db538ea964681158ba819e8bca3f131d5969
SDchao <wentaochens@outlook.com>
2020-03-15 11:48:43 +0800

    完成第一次实验

--- 200315/square_wave.m
+++ 200315/square_wave.m
@@ -1,11 +1,13 @@
 % squre_wave
-function sum = square_wave(n)
+function sums = square_wave(n)
     t = linspace(0, 4 * pi, 1001);
-    for k = 1: n
-        for i = 1 : 1001
-            t2 = t(k);
+    for i = 1 : 1001
+        sum = 0;
+        for k = 1 : n
+            t2 = t(i);
             tempSum = sin((2 * k - 1) * t2) / (2 * k - 1);
-            sum(k) = tempSum;
+            sum = sum + tempSum;
         end
+        sums(i) = sum;
     end
 end
